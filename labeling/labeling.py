02be9060642080587db33a93a4e998e16398fe28
pinglend <wes@getpinglend.com>
2022-04-26 19:42:40 -0700

    Improved how data is written and completed labeling and modeling

--- labeling/labeling.py
+++ labeling/labeling.py
@@ -1,36 +1,81 @@
 import json
-import csv
 import os
+import pandas as pd
+import shutil
 
 labeling_directory = os.listdir('data')
-f = open("data/data" + str(len(labeling_directory)) + ".csv", "w")
-writer = csv.writer(f)
 
 channels = ["CP3", "C3", "F5", "PO3", "PO4", "F6", "C4", "CP4"]
 header = [(channel + '_' + str(i)) for channel in channels for i in range(0, 16)]
 
 header.append("label")
-writer.writerow(header)
+df = pd.DataFrame(columns=header,)
 
 bw_dir = '../brainwave/data'
 key_dir = '../key/data'
 scroll_dir = '../scroll/data'
 
+def matrices_to_row(data):
+    row = []
+    for matrix in data:
+        for vector in matrix:
+            row.append(vector)
+    return row
+
+def create_label(string):
+    string = string.lower()
+    if string == "key.down" or string == "scroll_down":
+        return "down"
+    elif string == "key.up" or string == "scroll_up":
+        return "up"
+    else:
+        return "neutral"
+
+
+def load_list(dataset):
+    obj_list = []
+    for data in dataset:
+        obj = json.loads(data)
+        obj_list.append(obj)
+
+    return obj_list
+
+
 def label_data(bw_data,scroll_data,key_data):
-    print('label_data')
-    for scroll in scroll_data:
-        print('scroll')
-        obj = json.loads(scroll)
-        print(obj["input"])
-        # mouse_objs.append(obj)
 
-    for bw in bw_data:
-        print("yo")
-        # bw_obj = json.loads(bw[:-1])
-        # time = bw_obj["info"]['startTime'] / 1000
-        # data = bw_obj['data']
-        # print(time)
+    bw_objs = load_list(bw_data)
+    scroll_objs = load_list(scroll_data)
+    key_objs = load_list(key_data)
 
+    for bw_obj in bw_objs:
+        bw_time = bw_obj["info"]["startTime"] / 1000
+
+        for scroll_obj in scroll_objs:
+            scroll_time = scroll_obj["time"]
+
+            if abs(bw_time - scroll_time) < .1:
+                scroll_objs.remove(scroll_obj)
+
+                row = matrices_to_row(bw_obj["data"])
+                label = create_label(scroll_obj['input'])
+                if label:
+                    row.append(label)
+                    df.loc[len(df.index)] = row
+                break
+            else:
+                if len(df.query('label == "neutral"')) <  len(df.query('label == "down"')):
+                    row = matrices_to_row(bw_obj["data"])
+                    row.append("neutral")
+                    df.loc[len(df.index)] = row
+
+
+    neutral_count = len(df.query('label == "neutral"'))
+    down_count = len(df.query('label == "down"'))
+    up_count = len(df.query('label == "up"'))
+    data = df.drop(df.query('label == "neutral"').sample(frac=1-(down_count/neutral_count)).index)
+    if up_count > 0: data = df.drop(df.query('label == "up"').sample(frac=1 - (down_count / up_count)).index)
+
+    return data
 
 for filename in os.listdir(bw_dir):
 
@@ -41,104 +86,11 @@ for filename in os.listdir(bw_dir):
     scroll_data = open(scroll_dir + '/' + filename, "r")
     key_data = open(key_dir + '/' + filename, "r")
 
-    print(key_data)
-    label_data(bw_data,scroll_data,key_data)
+    data = label_data(bw_data,scroll_data,key_data)
 
+    shutil.move(bw_dir + '/' + filename, bw_dir + '/archive/' + filename)
+    shutil.move(scroll_dir + '/' + filename, scroll_dir + '/archive/' + filename)
+    shutil.move(key_dir + '/' + filename, key_dir + '/archive/' + filename)
 
-#
-# def matrices_to_row():
-#     row = []
-#     for matrix in data:
-#         for vector in matrix:
-#             row.append(vector)
-#     return row
-#
-# def create_label(string):
-#     string = string.lower()
-#     if string == "key.down" or string == "scroll_down":
-#         return "down"
-#     elif string == "key.up" or string == "scroll_up":
-#         return "up"
-#     else:
-#         return "neutral"
-#
-#
-# key_directory = 'data/key'
-# bw_directory = 'data/brainwave'
-# mouse_directory = 'data/mouse'
-# key_objs = []
-# #
-# # for filename in os.listdir(key_directory):
-# #     file = os.path.join(key_directory, filename)
-# #     # checking if it is a file
-# #     if os.path.isfile(file):
-# #         key_data = open(file, "r")
-# #         for line in key_data:
-# #             obj = json.loads(line)
-# #             key_objs.append(obj)
-#
-# mouse_objs = []
-# total_rows = 0
-#
-# for filename in os.listdir(mouse_directory):
-#     file = os.path.join(mouse_directory, filename)
-#     # checking if it is a file
-#     if os.path.isfile(file):
-#         mouse_data = open(file, "r")
-#         print(file)
-#         for line in mouse_data:
-#             obj = json.loads(line)
-#             mouse_objs.append(obj)
-#
-# print("Mouse objects count: " + str(len(mouse_objs)))
-#
-# mouse_count = 0
-# key_count = 0
-# neutral_count = 0
-#
-# for filename in os.listdir(bw_directory):
-#     file = os.path.join(bw_directory, filename)
-#     if os.path.isfile(file):
-#         bw_data = open(file, "r")
-#         print(file)
-#
-#         for bw in bw_data:
-#             bw_obj = json.loads(bw[:-2])
-#             time = bw_obj["info"]['startTime']/1000
-#             data = bw_obj['data']
-#
-#             mouse_found = False
-#             key_found = False
-#
-#             for key in key_objs:
-#                 if abs(key['time'] - time) < .01:
-#                     row = matrices_to_row(data)
-#                     label = create_label(key['key'])
-#                     if label:
-#                         key_count += 1
-#                         row.append(label)
-#                         total_rows += 1
-#                         writer.writerow(row)
-#                     key_found = True
-#
-#             for mouse in mouse_objs:
-#                 if abs(mouse['time'] - time) < .01:
-#                     row = matrices_to_row(data)
-#                     label = create_label(mouse['input'])
-#                     if label:
-#                         mouse_count += 1
-#                         row.append(label)
-#                         total_rows += 1
-#                         writer.writerow(row)
-#                     mouse_found = True
-#
-#             if not key_found and not mouse_found:
-#                 # if neutral_count < mouse_count:
-#                 row = matrices_to_row(data)
-#                 neutral_count += 1
-#                 row.append("neutral")
-#                 total_rows += 1
-#                 writer.writerow(row)
-#
-# print("Total rows: " + str(total_rows))
-# f.close()
+    data.to_pickle("data/data" + str(len(labeling_directory)) + ".pkl")
+
